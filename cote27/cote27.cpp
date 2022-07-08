11ff5c6970683970cff8853307df5606bb19adb7
ohne <ghdlr785@gmail.com>
2022-07-09 08:41:56 +0900

    초기 커밋.

---
+++ cote27/cote27.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+    answer.push_back(arr[0]);
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i - 1] != arr[i])   answer.push_back(arr[i]);
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
